bece6418c44847745dcd58bd8000b362f6dc9515
Benz <t.poonsanchai@gmail.com>
2023-10-01 17:02:11 +0700

    add local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Header from "./component/Header";
 import ShowTask from "./component/ShowTask";
@@ -7,8 +7,14 @@ import AddTask from "./component/AddTask";
 import "./App.css";
 
 function App() {
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState(
+    JSON.parse(localStorage.getItem("taskList")) || []
+  );
   const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(taskList));
+  }, [taskList]);
   return (
     <div className="App">
       <Header />
